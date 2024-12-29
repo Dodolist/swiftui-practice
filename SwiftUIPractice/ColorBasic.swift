ce37a8d51748d277bb0683af2881a0d592e1523b
Dodolist <dodolist3@gmail.com>
2024-12-29 19:30:46 +0900

    Shape

---
+++ SwiftUIPractice/ColorBasic.swift
@@ -0,0 +1,18 @@
+//
+//  ColorBasic.swift
+//  SwiftUIPractice
+//
+//  Created by 이종우 on 12/30/24.
+//
+
+import SwiftUI
+
+struct ColorBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorBasic()
+}
--- SwiftUIPractice/ShapeBasic.swift
+++ SwiftUIPractice/ShapeBasic.swift
@@ -9,7 +9,49 @@ import SwiftUI
 
 struct ShapeBasic: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView {
+            VStack(spacing: 20) {
+                Text("원형")
+                    .font(.title)
+                Circle()
+                    .stroke(Color.blue, style: StrokeStyle(lineWidth: 24, lineCap: .butt, dash: [2]))
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .padding()
+                
+                Text("타원형")
+                    .font(.title)
+                Ellipse()
+                    .fill(Color.blue)
+                    .frame(width: 200, height: 100, alignment: .center)
+                    .padding()
+                
+                Text("캡슐형")
+                    .font(.title)
+                Capsule(style: .continuous)
+                    .stroke(Color.blue, style: StrokeStyle(lineWidth: 8, lineCap: .round, dash: [16]))
+                    .frame(width: 200, height: 100, alignment: .center)
+                    .padding()
+            }
+            .frame(width: UIScreen.main.bounds.width)
+            .background(Color.gray.opacity(0.2))
+            
+            VStack(spacing: 20) {
+                Text("직사각형")
+                Rectangle()
+                    .frame(width: 200, height: 100, alignment: .center)
+                
+                Text("둥근 직사각형")
+                RoundedRectangle(cornerRadius: 24, style: .circular)
+                    .trim(from: 0.0, to: 0.7)
+                    .frame(width: 100, height: 200, alignment: .center)
+                RoundedRectangle(cornerRadius: 24, style: .continuous)
+                    .trim(from: 0.0, to: 0.3)
+                    .frame(width: 100, height: 200, alignment: .center)
+            }
+            .frame(width: UIScreen.main.bounds.width)
+            .background(Color.gray.opacity(0.1))
+        }
+        .tabViewStyle(.page(indexDisplayMode: .never))
     }
 }
 
