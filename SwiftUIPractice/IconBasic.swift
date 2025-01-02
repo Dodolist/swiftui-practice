653c922dd2e56a7319c9e87954bd7d7180ed1e30
Dodolist <dodolist3@gmail.com>
2025-01-02 15:17:26 +0900

    Icon

---
+++ SwiftUIPractice/IconBasic.swift
@@ -0,0 +1,18 @@
+//
+//  IconBasic.swift
+//  SwiftUIPractice
+//
+//  Created by 이종우 on 1/2/25.
+//
+
+import SwiftUI
+
+struct IconBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IconBasic()
+}
