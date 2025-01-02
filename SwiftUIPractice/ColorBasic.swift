1e8cb0b223b1e35291321c10792912bac740163d
Dodolist <dodolist3@gmail.com>
2025-01-02 14:59:52 +0900

    Color

--- SwiftUIPractice/ColorBasic.swift
+++ SwiftUIPractice/ColorBasic.swift
@@ -9,10 +9,44 @@ import SwiftUI
 
 struct ColorBasic: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Basic Color")
+                .font(.title)
+            RoundedRectangle(cornerRadius: 24)
+                .fill(Color.purple)
+                .frame(width: 200, height: 100, alignment: .center)
+            
+            // Primary Color
+            // 자동으로 다크모드 지원
+            Text("Primary Color")
+                .font(.title)
+            RoundedRectangle(cornerRadius: 24, style: .circular)
+                .fill(Color.primary)
+                .frame(width: 200, height: 100, alignment: .center)
+            RoundedRectangle(cornerRadius: 24, style: .circular)
+                .fill(Color.secondary)
+                .frame(width: 200, height: 100, alignment: .center)
+             
+            // UI Color
+            // UIKit에서 사용되는 Color
+            Text("UI Color")
+                .font(.title)
+            RoundedRectangle(cornerRadius: 24, style: .continuous)
+                .fill(Color(UIColor.secondarySystemBackground))
+                .frame(width: 200, height: 100, alignment: .center)
+            
+            // Custom Color
+            // Assets에 저장할 수 있음
+            Text("Custom Color")
+                .font(.title)
+            RoundedRectangle(cornerRadius: 24, style: .circular)
+                .fill(Color("--Primary-Blue"))
+                .frame(width: 200, height: 100)
+        }
     }
 }
 
 #Preview {
     ColorBasic()
+//        .preferredColorScheme(.dark)
 }
