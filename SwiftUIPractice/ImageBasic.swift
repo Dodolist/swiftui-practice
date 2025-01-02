bbfb4578c32e988c0a0e66c3abbab05a4b6a5475
Dodolist <dodolist3@gmail.com>
2025-01-02 15:44:25 +0900

    Image

---
+++ SwiftUIPractice/ImageBasic.swift
@@ -0,0 +1,18 @@
+//
+//  ImageBasic.swift
+//  SwiftUIPractice
+//
+//  Created by 이종우 on 1/2/25.
+//
+
+import SwiftUI
+
+struct ImageBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageBasic()
+}
